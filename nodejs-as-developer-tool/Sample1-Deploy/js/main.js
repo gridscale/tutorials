321ea52c618d216ce39b7fc249cb9000e9025337
Jan Stuhlmann <jan@gridscale.io>
2020-02-12 07:56:46 +0100

    extend example

--- nodejs-as-developer-tool/Sample1-Deploy/js/main.js
+++ nodejs-as-developer-tool/Sample1-Deploy/js/main.js
@@ -1,1 +1,9 @@
-var baz="foo";
+var gridscale = require('@gridscale/api').gridscale;
+var client = new gridscale.Client('xyz', 'abc');
+
+client.Server.list().then((result) => {
+    console.log(result);
+})
+.catch((e) => {
+    console.error(e);
+});
